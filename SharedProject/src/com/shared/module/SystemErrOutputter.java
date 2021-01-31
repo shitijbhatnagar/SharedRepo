14d6f3a3532865cf841b2af2a0a04c5f77e7a5ff
Shitij Bhatnagar <shitijbhatnagar@yahoo.com>
2021-01-31 22:43:30 +0530

    New file added

---
+++ SharedProject/src/com/shared/module/SystemErrOutputter.java
@@ -0,0 +1,10 @@
+package com.shared.module;
+
+public class SystemErrOutputter {
+
+	public static void main(String[] args) 
+	{
+		System.err.println("SystemErrOutputter program has run");
+	}
+
+}
