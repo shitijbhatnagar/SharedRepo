2866718f7d5188cc7ece99b24829d2b64127180c
Shitij Bhatnagar <shitijbhatnagar@yahoo.com>
2021-02-01 11:06:10 +0530

    one last minute change

--- SharedProject/src/com/shared/module/SystemGenericOutputter.java
+++ SharedProject/src/com/shared/module/SystemGenericOutputter.java
@@ -4,7 +4,7 @@ public class SystemGenericOutputter {
 
 	public static void main(String[] args) 
 	{
-		System.err.println("SystemGenericOutputter program has run now");
+		System.err.println("SystemGenericOutputter program has run now and it prints generic stuff only");
 	}
 
 }
